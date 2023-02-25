6f3c9e67f19a1af5ee7a5c60defba594902f9160
Ayush Gupta <77526719+ayush20501@users.noreply.github.com>
2023-02-25 16:16:53 +0530

    Update settings.py

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-!$lb+4-9)lxr%f917aabv0c#3$6bvg5ktw4_)s!$+!f3q*8e!o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.versel.app']
+ALLOWED_HOSTS = ['.versel.app','.now.sh']
 
 
 # Application definition
@@ -121,12 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / "static")
 
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
